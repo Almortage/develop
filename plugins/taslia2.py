f754cb4a896870807a0f89843db71fed8beb32d0
Almortagel <105941198+Almortage@users.noreply.github.com>
2024-01-09 16:38:10 +0200

    Update taslia2.py

--- plugins/taslia2.py
+++ plugins/taslia2.py
@@ -327,7 +327,7 @@ async def anem_1(c, msg):
         "😧",
         "😡",
         "😢",
-        "‎**تنصيب سورس كابوس**",
+        "‎**تنصيب سورس المرتجل**",
         "??",
         "😧",
         "😡",
@@ -623,7 +623,7 @@ async def man_1(c, msg):
 
 
 @Client.on_message(filters.command("مايكرو$", prefixes=f".") & filters.me)
-async def miكابوسo_1(c, msg):
+async def mialo_1(c, msg):
     animation_interval = 0.3
     animation_ttl = range(28)
     animation_chars = [
@@ -1334,21 +1334,21 @@ async def hak(client: Client, message: Message):
   asyncio.sleep(2)
   await message.edit_text("Hacking... 42.17%\n[███████░░░░░░░░░░░░░]\nSearching for databases\nETA: 0m, 12s")
   asyncio.sleep(2)
-  await message.edit_text("Hacking... 55.30%\n[█████████░░░░░░░░░░░]\nFound msgstore.db.كابوسypt12\nETA: 0m, 10s")
+  await message.edit_text("Hacking... 55.30%\n[█████████░░░░░░░░░░░]\nFound msgstore.db.المرتجلypt12\nETA: 0m, 10s")
   asyncio.sleep(2)
-  await message.edit_text("Hacking... 64.86%\n[███████████░░░░░░░░░]\nFound msgstore.db.كابوسypt12\nETA: 0m, 08s")
+  await message.edit_text("Hacking... 64.86%\n[███████████░░░░░░░░░]\nFound msgstore.db.المرتجلypt12\nETA: 0m, 08s")
   asyncio.sleep(2)
-  await message.edit_text("Hacking... 74.02%\n[█████████████░░░░░░░]\nTrying to Deكابوسypt...\nETA: 0m, 06s")
+  await message.edit_text("Hacking... 74.02%\n[█████████████░░░░░░░]\nTrying to Deالمرتجلypt...\nETA: 0m, 06s")
   asyncio.sleep(2)
-  await message.edit_text("Hacking... 86.21%\n[███████████████░░░░░]\nTrying to Deكابوسypt...\nETA: 0m, 04s")
+  await message.edit_text("Hacking... 86.21%\n[███████████████░░░░░]\nTrying to Deالمرتجلypt...\nETA: 0m, 04s")
   asyncio.sleep(2)
-  await message.edit_text("Hacking... 93.50%\n[█████████████████░░░]\nDeكابوسyption successful!\nETA: 0m, 02s")
+  await message.edit_text("Hacking... 93.50%\n[█████████████████░░░]\nDeالمرتجلyption successful!\nETA: 0m, 02s")
   asyncio.sleep(2)
   await message.edit_text("Hacking... 100%\n[████████████████████]\nScanning file...\nETA: 0m, 00s")
   asyncio.sleep(2)
   await message.edit_text("Hacking complete!\nUploading file...")
   asyncio.sleep(2)
-  await message.edit_text("Targeted Account Hacked...!\n\n ✅ File has been successfully uploaded to my server.\nWhatsApp Database:\n./DOWNLOADS/msgstore.db.كابوسypt12")
+  await message.edit_text("Targeted Account Hacked...!\n\n ✅ File has been successfully uploaded to my server.\nWhatsApp Database:\n./DOWNLOADS/msgstore.db.المرتجلypt12")
  
 # == == == == == == == == == == == == == == == == == == == ==
 
@@ -1726,7 +1726,7 @@ async def gkahgagw(client: Client, message: Message):
     ]
     if message.forward_from:
         return
-    await message.edit("كابوس..كابوس..كابوس..كابوس")
+    await message.edit("المرتجل..المرتجل..المرتجل..المرتجل")
     await asyncio.sleep(4)
     for i in animation_ttl:
         await asyncio.sleep(animation_interval)
@@ -2054,7 +2054,7 @@ async def gahite(client: Client, message: Message):
 
 # == == == == == == == == == == == == == == == == == == == ==
 
-@Client.on_message(filters.command(["كابوس", "سورس كابوس", "السورس"], ".") & filters.me)
+@Client.on_message(filters.command(["المرتجل", "سورس المرتجل", "السورس"], ".") & filters.me)
 async def gajjajay(client: Client, message: Message):
     await message.edit("هلو")
     await asyncio.sleep(0.3)
@@ -2062,7 +2062,7 @@ async def gajjajay(client: Client, message: Message):
     await asyncio.sleep(0.2)
     await message.edit("هتشوف الجمدان كلو")
     await asyncio.sleep(0.5)
-    await message.edit("كابوس بتعمل عظمه ")
+    await message.edit("المرتجل بتعمل عظمه ")
     await asyncio.sleep(0.2)
     await message.edit("فوق")
     await asyncio.sleep(0.3)
@@ -2070,7 +2070,7 @@ async def gajjajay(client: Client, message: Message):
     await asyncio.sleep(0.3)
     await message.edit("🔥🔥🔥")
     await asyncio.sleep(0.3)
-    await message.edit("هلو هنا هتشوف الجمدان كلو كابوس بتعمل عظمه فوق اي حد 🔥🔥🔥")
+    await message.edit("هلو هنا هتشوف الجمدان كلو المرتجل بتعمل عظمه فوق اي حد 🔥🔥🔥")
 
 # == == == == == == == == == == == == == == == == == == == ==
 
@@ -2385,127 +2385,3 @@ async def fela(client: Client, message: Message):
         await asyncio.sleep(animation_interval)
         await message.edit(animation_chars[i % 6])       
         
-         
-
-@Client.on_message(filters.command("تهكير$", ".") & filters.me)
-async def fela(client: Client, message: Message):
-    info={
-        "header": "Fun hack animation.",
-        "description": "Reply to user to show hack animation",
-        "note": "This is just for fun. Not real hacking.",
-        "usage": "{tr}hack",
-    },
-async def _(event):
-    "Fun hack animation."
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        idd = reply_message.sender_id
-        if idd == 5089553588:
-            await edit_or_reply(
-                event, "**᯽︙ عـذرا لا استـطيع اخـتراق مـطوري اعـتذر او سيقـوم بتهـكيرك**"
-            )
-        else:
-            event = await edit_or_reply(event, "يتـم الاختـراق ..")
-            animation_chars = [
-                "᯽︙ تـم الربـط بسـيرفرات الـتهكير الخـاصة",
-                "تـم تحـديد الضحـية",
-                "**تهكيـر**... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ",
-                "**تهكيـر**... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ",
-                "**تهكيـر**... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ",
-                "**تهكيـر**... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ",
-                "**تهكيـر**... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ",
-                "**تهكيـر**... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ ",
-                "**تهكيـر**... 84%\n█████████████████████▒▒▒▒ ",
-                "**تهكيـر**... 100%\n████████████████████████ ",
-                f"᯽︙ ** تـم اخـتراق الضـحية**..\n\nقـم بالـدفع الى {USER_NAME} لعـدم نشـر معلوماتك وصـورك",
-            ]
-            animation_interval = 3
-            animation_ttl = range(11)
-            for i in animation_ttl:
-                await asyncio.sleep(animation_interval)
-                await event.edit(animation_chars[i % 11])
-    else:
-        await edit_or_reply(
-            event,
-            "᯽︙ لم يتـم التعـرف على المستـخدم",
-            parse_mode=_format.parse_pre,
-        )
-@jepiq.ar_cmd(
-    pattern="تهكير2$",
-    command=("تهكير2", plugin_category),
-    info={
-        "header": "Fun hack animation.",
-        "description": "Reply to user to show hack animation",
-        "note": "This is just for fun. Not real hacking.",
-        "usage": "{tr}hack",
-    },
-)
-async def _(event):
-    if event.fwd_from:
-        return
-
-    animation_interval = 3
-
-    animation_ttl = range(11)
-
-    
-    await event.edit("**جارِ الاختراق الضحية..**")
-
-    animation_chars = [
-        
-            "**جار تحديد الضحية...**",
-            "**تم تحديد الضحية بنجاح ✓**",
-            "`يتم الاختراق... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`يتم الاختراق... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`يتم الاختراق... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
-            "`يتم الاختراق... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`يتم الاختراق... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`يتم الاختراق... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`يتم الاختراق... 84%\n█████████████████████▒▒▒▒ `",
-            "`يتم الاختراق... 100%\n████████████████████████ `",
-            "` تم رفع معلومات الشخص...\n\nسيتم ربط المعلومات بسيرفرات التهكير الخاصه..`"
-        ]
-  
-    for i in animation_ttl:
-
-        await asyncio.sleep(animation_interval)
-# Made for Hussein
-        await event.edit(animation_chars[i % 11])
-
-    await asyncio.sleep(2)
-
-    animation_interval = 1
-    animation_ttl = range(0,14)
-    await event.edit("**يتم الاتصال لسحب التوكن الخاص به عبر موقع.telegram.org**")
-    await asyncio.sleep(1)
-    animation_chars = [
-           "root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...\nيتم الان الرفع عبر CMD تلقائياً ...\n\nroot@anon:~# trap whoami\n\nwhoami=user\nboost_trap on force ...\nvictim detected  in ghost ...\n\nتم اكتمال العملية ✓!\nيتم الان استخراج توكن الضحية!\nToken=`DJ65gulO90P90nlkm65dRfc8I`",
-         ]
-            
-
-    for i in animation_ttl:
-# Made for Hussein        
-        await asyncio.sleep(animation_interval)
-        await event.edit(animation_chars[i % 14])
-    
-    await asyncio.sleep(2)
-
-    await event.edit("`starting telegram hack`")
-    await asyncio.sleep(2)
-    await event.edit("`يتم سحب الصور والمعلومات...\n 88%completed\nall data from telegram private server downloaded\nterminal download sucessfull--with status jh3233fdg66y yr4vv.irh\n data collected from tg-bot\nTERMINAL:\n Bruteforce-Telegram-0.1.tar.gz (1.3)downloaded`")
-    await asyncio.sleep(5)
-    await event.edit("`100%\n█████████████████████████ `\n\n\n  TERMINAL:\nيتم تنزيل Bruteforce-Telegram-0.1.tar.gz (9.3 kB)\nCollecting Data Package\n  يتم تنزيل Telegram-Data-Sniffer-7.1.1-py2.py3-none-any.whl (82 kB)\nBuilding wheel for Tg-Bruteforcing (setup.py): finished with status 'done'\nCreated wheel for telegram: filename=Telegram-Data-Sniffer-0.0.1-py3-none-any.whl size=1306 sha256=cb224caad7fe01a6649188c62303cd4697c1869fa12d280570bb6ac6a88e6b7e\n  Stored in directory: `")
-    await asyncio.sleep(5)
-    await event.edit(f"`تم سحب جميع معلومات الحساب\n قم بلدفع الى {ALIVE_NAME} 100$ \n حتى لا يقم بنشر صورك ومحادثاتك !`")
-    await asyncio.sleep(5)
-    h=(random.randrange(1,5)) 
-    if h==1:
-        await event.edit("`تم رفع جميع الصور المخزونة والمحادثات وجهات اتصال المستخدم عبر مجلد pdf \n\n😂 لا تقلق انا فقط من ارئ معلوماتك 😎😎.. اذا لم تصدق ادخل الى هذا الرابط وانظر بنفسك` 🙂\n\nhttps://drive.google.com/file/d/1EHJSkt64RZEw7a2h8xkRqZSv_4dWhB02/view?usp=sharing")
-    if h==2:
-        await event.edit("`تم رفع جميع الصور المخزونة والمحادثات وجهات اتصال المستخدم عبر مجلد pdf \n\n😂 لا تقلق انا فقط من ارئ معلوماتك 😎😎.. اذا لم تصدق ادخل الى هذا الرابط وانظر بنفسك` 🙂\n\nhttps://drive.google.com/file/d/1YaUfNVrHU7zSolTuFN3HyHJuTWQtdL2r/view?usp=sharing")
-    if h==3:
-        await event.edit("`تم رفع جميع الصور المخزونة والمحادثات وجهات اتصال المستخدم عبر مجلد pdf \n\n😂 لا تقلق انا فقط من ارئ معلوماتك 😎😎.. اذا لم تصدق ادخل الى هذا الرابط وانظر بنفسك` 🙂\n\nhttps://drive.google.com/file/d/1o2wXirqy1RZqnUMgsoM8qX4j4iyse26X/view?usp=sharing")
-    if h==4:
-        await event.edit("`تم رفع جميع الصور المخزونة والمحادثات وجهات اتصال المستخدم عبر مجلد pdf \n\n😂 لا تقلق انا فقط من ارئ معلوماتك 😎😎.. اذا لم تصدق ادخل الى هذا الرابط وانظر بنفسك` 🙂\n\nhttps://drive.google.com/file/d/15-zZVyEkCFA14mFfD-2DKN-by1YOWf49/view?usp=sharing")
-    if h==5:
-        await event.edit("`تم رفع جميع الصور المخزونة والمحادثات وجهات اتصال المستخدم عبر مجلد pdf \n\n😂 لا تقلق انا فقط من ارئ معلوماتك 😎😎.. اذا لم تصدق ادخل الى هذا الرابط وانظر بنفسك` 🙂\n\nhttps://drive.google.com/file/d/1hPUfr27UtU0XjtC20lXjY9G3D9jR5imj/view?usp=sharing")
