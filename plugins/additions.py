a70116907059433cb901124ee796d9a3b648617d
Almortagel <105941198+Almortage@users.noreply.github.com>
2024-01-09 19:20:07 +0200

    Update additions.py

--- plugins/additions.py
+++ plugins/additions.py
@@ -70,6 +70,14 @@ async def commands(c,msg):
   except :
     await msg.edit("• فعل الانلاين من @botFather")
 
+@Client.on_message(filters.command(["ابراج$","الابراج$"],prefixes=f".") & filters.me )
+async def commands(c,msg):
+  try :
+    result = await c.get_inline_bot_results(bot_user,query="الابراج")
+    await msg.delete()
+    await c.send_inline_bot_result(msg.chat.id, result.query_id, result.results[0].id)
+  except :
+    await msg.edit("• فعل الانلاين من @botFather")
 
 @Client.on_message(filters.command(["سورس$","السورس$"],prefixes=f".") & filters.me )
 async def source(c,msg):
