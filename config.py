6246a5561155b908cccbcd807e4422713de69989
Almortagel <105941198+Almortage@users.noreply.github.com>
2024-01-10 21:55:27 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ r = redis.Redis(
   port=17811,
   password='y41sFD7N8cY5Ob2MGPZkGdrTndVFY92h')
 
-sudo_id = 6581896306
+sudo_id = 5089553588
 bot_user = Mody.BOT_USER
 via_user = Mody.VIA_USER
 elhyba = bot_user
@@ -16,8 +16,8 @@ api_id = Mody.APP_ID
 api_hash = Mody.API_HASH
 session = Mody.SESSION
 token = Mody.TG_BOT_TOKEN
-sudo_command = [6581896306]
-pm = "6581896306"
+sudo_command = [5089553588]
+pm = "5089553588"
 mention = Mody.MENTION
 plugins = dict(root="plugins")
 app = Client(via,api_id , api_hash ,in_memory=True,session_string = session,plugins=plugins)
